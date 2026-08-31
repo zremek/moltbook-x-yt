d931c5f7a72f3a0bad5e90a90817a32ac82c31c6
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2026-08-31 11:09:22 +0200

    dataviz done

--- 03_dataviz_esa.R
+++ 03_dataviz_esa.R
@@ -37,15 +37,41 @@ both_esa |> count(source)
 ggplot(both_esa, aes(x = created_at, colour = source)) +
   geom_freqpoly(binwidth = 60 * 60 * 24 * 7) +  # weekly
   theme_minimal() + 
-  scale_x_datetime(date_breaks = "1 month", date_labels = "%b")
+  scale_x_datetime(date_breaks = "1 month", date_labels = "%b") 
+
+ggplot(both_esa, aes(x = created_at, colour = source)) +
+  geom_freqpoly(binwidth = 60 * 60 * 24) +  # daily
+  theme_minimal() + 
+  scale_x_datetime(date_breaks = "1 month", date_labels = "%b") 
 
 # stacked bars for classification #### 
 
 plot_grpfrq(both_esa$fake_or_real_class, 
-            both_esa$source, bar.pos = "stack", )
+            both_esa$source, bar.pos = "stack")
 
 plot_xtab(both_esa$source, 
           both_esa$fake_or_real_class, 
           bar.pos = "stack", 
-          margin = "row") + 
-  coord_flip() + theme_minimal()
+          margin = "row", show.n = F, rev.order = T) + 
+  coord_flip() + 
+  theme_minimal()
+
+# TODO add proper labels, don't adjust colours
+
+plot_xtab(both_esa$source, 
+          both_esa$fake_or_real_class, 
+          bar.pos = "stack", 
+          margin = "row", show.n = F, rev.order = T, 
+          legend.labels = c("no", "yes", "uncertain"), 
+          legend.title = "content discusses Moltbook as real or fake?") + 
+  coord_flip() + 
+  theme_minimal() + 
+  labs(caption = "own elaboration")
+
+
+ggplot(both_esa, aes(x = created_at, colour = source)) +
+  geom_freqpoly(binwidth = 60 * 60 * 24) +  # daily
+  theme_minimal() + 
+  scale_x_datetime(date_breaks = "1 month", date_labels = "%b") + 
+  labs(x = "content created at [2026 daily series]", 
+       caption = "own elaboration")
